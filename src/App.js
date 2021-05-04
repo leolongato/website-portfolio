1e0ce623a54142fc05c2d91c02108159152d232c
leolongato <leonardolongato@gmail.com>
2021-05-03 23:28:06 -0300

    Redirecting to the new version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import MenuBar from './components/menu/index';
-import GlobalStyle from './Global/GlobalStyle';
+import React from "react";
+import styled from "styled-components";
+// import MenuBar from "./components/menu/index";
+// import GlobalStyle from "./Global/GlobalStyle";
 
 //import DemoCarousel from './components/carousel/index';
 
 function App() {
   return (
-    <>
-        <MenuBar />
-        <GlobalStyle />
-    </>
+    // <>
+    <Container>
+      <h1>Este site foi descontinuado.</h1>
+      <a href="https://leonardolongato.vercel.app">
+        Clique aqui para acessar a nova versão.
+      </a>
+    </Container>
+    //<MenuBar />
+    //<GlobalStyle />
+    // </>
   );
 }
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
